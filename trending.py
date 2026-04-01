2159bd7a78787e24455470b316a48f048a04e788
samarth259 <71771489+samarth259@users.noreply.github.com>
2026-04-02 01:08:51 +0530

    Add function to get trending YouTube videos

--- trending.py
+++ trending.py
@@ -1,1 +1,15 @@
+from googleapiclient.discovery import build
+import os
 
+def get_trending():
+    youtube = build('youtube', 'v3', developerKey=os.getenv("YOUTUBE_API_KEY"))
+
+    request = youtube.videos().list(
+        part='snippet',
+        chart='mostPopular',
+        regionCode='IN',
+        maxResults=1
+    )
+
+    response = request.execute()
+    return response['items'][0]['snippet']['title']
