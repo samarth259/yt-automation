38e3d2d6239a6d99f9db77bdbeb56e1e2df108c2
samarth259 <71771489+samarth259@users.noreply.github.com>
2026-04-02 01:10:33 +0530

    Implement create_video function with ffmpeg command
    Add function to create a video using ffmpeg.

--- video.py
+++ video.py
@@ -1,1 +1,8 @@
+import os
 
+def create_video():
+    os.system("""
+    ffmpeg -loop 1 -i assets/bg.jpg -i voice.mp3 \
+    -c:v libx264 -t 30 -pix_fmt yuv420p \
+    -vf scale=1080:1920 output.mp4
+    """)
